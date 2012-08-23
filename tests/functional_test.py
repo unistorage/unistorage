5a81c8b23f1d21ac7318382571996db0bd60e313
Anton Romanovich <anthony.romanovich@gmail.com>
2012-08-24 01:16:28 +0600

    Улучшен functional_test.py

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -1,32 +1,40 @@
 import sys
 import time
 import unittest
+import os
 from pprint import pprint
-
 from bson.objectid import ObjectId
-import requests
+
+from webtest import TestApp
+import redis
+from rq import Queue, Worker, use_connection
 
 import settings
 import app
 
-
 class FunctionalTest(unittest.TestCase):
-    def setUp(self):
-        try:
-            self.port = int(sys.argv[1])
-        except:
-            self.port = 5000
+    @classmethod
+    def setUpClass(cls):
+        cls.app = TestApp(app.app)
+        cls.headers = {'Token': settings.TOKENS[0]}
+        cls.db = app.get_mongodb_connection()[settings.MONGO_DB_NAME]
 
-        self.base_url = 'http://127.0.0.1:%d' % self.port
-        self.headers = {'Token': settings.TOKENS[0]}
-        self.db = app.get_mongodb_connection()[settings.MONGO_DB_NAME]
+    def run_worker(self):
+        old_stderr = sys.stderr
+        sys.stderr = sys.stdout # Let worker log be captured by nose
 
+        use_connection(redis.Redis())
+        queues = map(Queue, ['default'])
+        w = Worker(queues)
+        w.work(burst=True)
+
+        sys.stderr = old_stderr
+   
     def put_file(self, path):
-        files = {'file': open(path, 'rb')}
-        r = requests.post(self.base_url, files=files, headers=self.headers)
+        files = [('file', os.path.basename(path), open(path, 'rb').read())]
+        r = self.app.post('/', headers=self.headers, upload_files=files)
         self.assertEquals(r.json['status'], 'ok')
         self.assertTrue('id' in r.json)
-
         return ObjectId(r.json['id'])
 
     def assert_mimetype(self, r, value):
@@ -36,7 +44,7 @@ class FunctionalTest(unittest.TestCase):
         self.assertEquals(r.json['information']['fileinfo'][key], value)
 
     def check(self, url, width=None, height=None, mime=None):
-        r = requests.get(url, headers=self.headers)
+        r = self.app.get(url, headers=self.headers)
         self.assertEquals(r.json['status'], 'ok')
         if width:
             self.assert_fileinfo(r, 'width', width)
@@ -49,24 +57,24 @@ class FunctionalTest(unittest.TestCase):
     def test_resize_keep_jpg(self):
         original_id = self.put_file('./tests/images/some.jpeg')
 
-        url = '%s/%s/' % (self.base_url, original_id)
+        url = '/%s/' % original_id
         self.check(url, width=640, height=480, mime='image/jpeg')
         
         resize_action_url = url + '?action=resize&mode=keep&w=400'
-        r = requests.get(resize_action_url, headers=self.headers)
+        r = self.app.get(resize_action_url, headers=self.headers)
         self.assertEquals(r.json['status'], 'ok')
         resized_image_id = ObjectId(r.json['id'])
 
-        resized_image_url = '%s/%s/' % (self.base_url, resized_image_id)
-        r = requests.get(resized_image_url, headers=self.headers)
+        resized_image_url = '/%s/' % resized_image_id
+        r = self.app.get(resized_image_url, headers=self.headers)
         self.assertTrue('finish_time' in r.json)
 
         # Make sure that consequent calls return the same id for the same action
-        r = requests.get(resize_action_url, headers=self.headers)
+        r = self.app.get(resize_action_url, headers=self.headers)
         self.assertEquals(r.json['status'], 'ok')
         self.assertEquals(ObjectId(r.json['id']), resized_image_id)
         
-        time.sleep(1)
+        self.run_worker()
         # Make sure that original has resized image in modifications
         # and resized image points to it's original.
         resized_image = self.db.fs.files.find_one(resized_image_id)
@@ -80,41 +88,40 @@ class FunctionalTest(unittest.TestCase):
     def test_convert_jpg_to_gif(self):
         original_id = self.put_file('./tests/images/some.jpeg')
 
-        url = '%s/%s/' % (self.base_url, original_id)
+        url = '/%s/' % original_id
         self.check(url, width=640, height=480, mime='image/jpeg')
         
         convert_action_url = url + '?action=convert&to=image/gif'
-        r = requests.get(convert_action_url, headers=self.headers)
-        pprint(r.json)
+        r = self.app.get(convert_action_url, headers=self.headers)
         self.assertEquals(r.json['status'], 'ok')
 
-        time.sleep(1)
+        self.run_worker()
 
-        converted_image_url = '%s/%s/' % (self.base_url, r.json['id'])
+        converted_image_url = '/%s/' % r.json['id']
         r = self.check(converted_image_url, width=640, height=480, mime='image/gif')
         self.assertTrue('finish_time' not in r.json)
 
     def test_make_grayscale(self):
         original_id = self.put_file('./tests/images/some.png')
 
-        url = '%s/%s/' % (self.base_url, original_id)
+        url = '/%s/' % original_id
         self.check(url, width=43, height=43, mime='image/png')
 
         grayscale_action_url = url + '?action=make_grayscale'
-        r = requests.get(grayscale_action_url, headers=self.headers)
+        r = self.app.get(grayscale_action_url, headers=self.headers)
         
-        time.sleep(1)
+        self.run_worker()
 
-        grayscaled_image_url = '%s/%s/' % (self.base_url, r.json['id'])
+        grayscaled_image_url = '/%s/' % r.json['id']
         self.check(grayscaled_image_url, width=43, height=43, mime='image/png')
 
     def test_validation_errors(self):
         original_id = self.put_file('./tests/images/some.png')
 
-        url = '%s/%s/' % (self.base_url, original_id)
+        url = '/%s/' % original_id
 
-        r = requests.get(url + '?action=lalala', headers=self.headers)
+        r = self.app.get(url + '?action=lalala', headers=self.headers, status='*')
         self.assertEquals(r.json['status'], 'error')
 
-        r = requests.get(url + '?action=convert&to=lalala', headers=self.headers)
+        r = self.app.get(url + '?action=convert&to=lalala', headers=self.headers, status='*')
         self.assertEquals(r.json['status'], 'error')
