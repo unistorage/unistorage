a404c67c8cba7a59a1c256202472f5ff8126d1ab
Anton Romanovich <anthony.romanovich@gmail.com>
2012-11-09 13:03:03 +0600

    Improve video actions validation

---
+++ actions/videos/common_validation.py
@@ -0,0 +1,23 @@
+from actions.utils import ValidationError
+from actions.videos.avconv import get_codec_supported_actions
+
+
+def validate_source(source_file, require_encoding_support=False):
+    data = source_file.fileinfo
+    if not data['video'] or not data['audio']:
+        raise ValidationError('Source video file must contain at least one '
+                              'audio and video stream')
+    
+    acodec_name = data['audio']['codec']
+    acodec = get_codec_supported_actions('audio', acodec_name)
+    if not acodec or not acodec['decoding'] or \
+            (require_encoding and not acodec['encoding']):
+        raise ValidationError('Sorry, we can\'t handle audio stream '
+                              'encoded using %s' % acodec_name)
+    
+    vcodec_name = data['video']['codec']
+    vcodec = get_codec_supported_actions('video', vcodec_name)
+    if not vcodec or not vcodec['decoding'] or \
+            (require_encoding and not vcodec['encoding']):
+        raise ValidationError('Sorry, we can\'t handle video stream '
+                              'encoded using %s' % vcodec_name)
