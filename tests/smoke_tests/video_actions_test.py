15e8f2c1560f06e28e6fb9394300566a1cf65875
Anton Romanovich <anthony.romanovich@gmail.com>
2012-11-19 12:30:09 +0600

    Uncomment video tests

--- tests/smoke_tests/video_actions_test.py
+++ tests/smoke_tests/video_actions_test.py
@@ -16,43 +16,43 @@ class Test(SmokeTest):
     def setUpClass(cls):
         super(Test, cls).setUpClass(TEST_SOURCE_DIR, TEST_TARGET_DIR)
 
-   #def test_convert(self):
-        #results_dir = os.path.join(TEST_TARGET_DIR, 'convert')
-        #os.makedirs(results_dir)
+   def test_convert(self):
+        results_dir = os.path.join(TEST_TARGET_DIR, 'convert')
+        os.makedirs(results_dir)
 
-        #convert_targets = {
-            #'ogg': {'vcodec': ['theora'], 'acodec': 'vorbis'},
-            #'webm': {'vcodec': ['vp8'], 'acodec': 'vorbis'},
-            #'flv': {'vcodec': ['h264', 'flv'], 'acodec': 'mp3'},
-            #'mp4': {'vcodec': ['h264', 'divx'], 'acodec': 'mp3'},
-            #'mkv': {'vcodec': ['h263', 'mpeg1', 'mpeg2'], 'acodec': 'mp3'},
-        #}
+        convert_targets = {
+            'ogg': {'vcodec': ['theora'], 'acodec': 'vorbis'},
+            'webm': {'vcodec': ['vp8'], 'acodec': 'vorbis'},
+            'flv': {'vcodec': ['h264', 'flv'], 'acodec': 'mp3'},
+            'mp4': {'vcodec': ['h264', 'divx'], 'acodec': 'mp3'},
+            'mkv': {'vcodec': ['h263', 'mpeg1', 'mpeg2'], 'acodec': 'mp3'},
+        }
 
-        #for format in convert_targets:
-            #acodec = convert_targets[format]['acodec']
-            #for vcodec in convert_targets[format]['vcodec']:
-                #for source_name, source_file in self.source_files():
-                    #print '.', source_name
-                    #result, ext = convert(source_file, format, vcodec, acodec, only_try=True)
+        for format in convert_targets:
+            acodec = convert_targets[format]['acodec']
+            for vcodec in convert_targets[format]['vcodec']:
+                for source_name, source_file in self.source_files():
+                    print '.', source_name
+                    result, ext = convert(source_file, format, vcodec, acodec, only_try=True)
 
-                    #target_name = '%s_using_vcodec_%s_acodec_%s.%s' % \
-                        #(source_name, vcodec, acodec, ext)
-                    #target_path = os.path.join(results_dir, target_name)
-                    #with open(target_path, 'w') as target_file:
-                        #target_file.write(result.read())
+                    target_name = '%s_using_vcodec_%s_acodec_%s.%s' % \
+                        (source_name, vcodec, acodec, ext)
+                    target_path = os.path.join(results_dir, target_name)
+                    with open(target_path, 'w') as target_file:
+                        target_file.write(result.read())
 
-    #def test_watermark(self):
-        #results_dir = os.path.join(TEST_TARGET_DIR, 'watermark')
-        #os.makedirs(results_dir)
+    def test_watermark(self):
+        results_dir = os.path.join(TEST_TARGET_DIR, 'watermark')
+        os.makedirs(results_dir)
 
-        #for source_name, source_file in self.source_files():
-            #watermark_file = open(os.path.join(fixture_path('watermarks'), 'watermark.png'))
-            #result, ext = watermark(source_file, watermark_file, 100, 100, 10, 10, 'ne')
+        for source_name, source_file in self.source_files():
+            watermark_file = open(os.path.join(fixture_path('watermarks'), 'watermark.png'))
+            result, ext = watermark(source_file, watermark_file, 100, 100, 10, 10, 'ne')
 
-            #target_name = '%s_w_watermark.%s' % (source_name, ext)
-            #target_path = os.path.join(results_dir, target_name)
-            #with open(target_path, 'w') as target_file:
-                #target_file.write(result.read())
+            target_name = '%s_w_watermark.%s' % (source_name, ext)
+            target_path = os.path.join(results_dir, target_name)
+            with open(target_path, 'w') as target_file:
+                target_file.write(result.read())
 
     def test_extract_audio(self):
         results_dir = os.path.join(TEST_TARGET_DIR, 'watermark')
