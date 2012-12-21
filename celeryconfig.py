2ad9ab7e0b7e0368f62d488c3282bfaf13a91abb
Anton Romanovich <anthony.romanovich@gmail.com>
2012-12-21 16:58:46 +0600

    Fix

--- celeryconfig.py
+++ celeryconfig.py
@@ -6,6 +6,6 @@ BROKER_URL = settings.CELERY_BROKER
 # List of modules to import when celery starts.
 CELERY_IMPORTS = ('actions.tasks',)
 
-ADMINS = ('Anton Romanovich', 'anthony.romanovich@gmail.com')
+ADMINS = [('Anton Romanovich', 'anthony.romanovich@gmail.com')]
 
 CELERY_SEND_TASK_ERROR_EMAILS = True
