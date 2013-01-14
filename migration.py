8d8dc5268fff3f7b8adccba1f128b7c588480bc6
Anton Romanovich <anthony.romanovich@gmail.com>
2013-01-14 14:18:11 +0600

    Update migration.py

--- migration.py
+++ migration.py
@@ -9,10 +9,11 @@ with app.app_context():
         #'unistorage_type': 'video',
         #'extra.audio.codec': 'vorbis'
     })
-    fields_to_update = ['extra.audio.channels', 'content_type']
+    fields_to_update = ['extra.video.codec']
 
     for file in list(files_to_update):
         _id = file['_id']
+        print 'Processing %s' % _id
 
         gridout = fs.get(_id)
         file_data = file_utils.get_file_data(gridout, file['filename'])
@@ -23,14 +24,25 @@ with app.app_context():
 
             target_dict = set_spec
             source_dict = file_data
+            original_dict = file
             for part in parts[:-1]:
                 source_dict = source_dict[part]
+                original_dict = original_dict[part]
                 target_dict.setdefault(part, {})
                 target_dict = target_dict[part]
-
-            target_dict[parts[-1]] = source_dict[parts[-1]]
+            
+            last_part = parts[-1]
+            something_has_changed = False
+            if source_dict[last_part] == original_dict[last_part]:
+                print '  Field `%s` has not changed.' % field
+            else:
+                something_has_changed = True
+            if not something_has_changed:
+                print '  Nothing has changed!'
+            target_dict[last_part] = source_dict[last_part]
         
         if 'content_type' in fields_to_update:
             set_spec['contentType'] = set_spec.pop('content_type')
 
         db.fs.files.update({'_id': _id}, {'$set': set_spec})
+        print '  OK'
