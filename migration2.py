8bd773939595a6bd28abd681071a1f9cfba720ed
Anton Romanovich <anthony.romanovich@gmail.com>
2013-01-15 13:46:29 +0600

    Log failed fixes during migration

--- migration2.py
+++ migration2.py
@@ -8,15 +8,14 @@ from app import db, fs
 from wsgi import app
 
 
-def validate_extra(_file):
-    unistorage_type = _file.get('unistorage_type')
+def validate_extra(unistorage_type, extra):
     if not unistorage_type:
         raise Exception('File doesn\'t have `unistorage_type`')
     elif unistorage_type in ('video', 'audio', 'image'):
         with open('schemas/%s.json' % unistorage_type) as schema_file:
             schema = json.load(schema_file)
             extra_schema = schema['properties']['data']['properties']['extra']
-            jsonschema.validate(_file.get('extra', {}), extra_schema)
+            jsonschema.validate(extra, extra_schema)
 
 
 with app.app_context():
@@ -27,25 +26,39 @@ with app.app_context():
     })
 
     count = files_to_update.count()
-
+    log = open('./failed_fixes.txt', 'a+')
     for i, _file in enumerate(files_to_update):
         _id = _file['_id']
         print 'Processing %s (%i/%i)' % (_id, i, count)
         try:
-            validate_extra(_file)
+            unistorage_type = _file.get('unistorage_type')
+            extra = _file.get('extra', {})
+            validate_extra(unistorage_type, extra)
         except Exception as e:
             print '  Schema validation failed:'
             print '  %s: %s' % (e, getattr(e, 'path', ''))
             gridout = fs.get(_id)
             new_file_data = file_utils.get_file_data(gridout, _file['filename'])
 
-            set_spec = {
-               '$set': {
-                    'unistorage_type': new_file_data['unistorage_type'],
-                    'extra': new_file_data['extra'],
+            new_unistorage_type = new_file_data['unistorage_type']
+            new_extra = new_file_data['extra']
+            try:
+                validate_extra(new_unistorage_type, new_extra)
+            except Exception as e:
+                print '  Not fixed!'
+                message = '  %s: %s' % (e, getattr(e, 'path', ''))
+                print message
+                log.write('%s %s\n' % (_id, message))
+            else:
+                set_spec = {
+                   '$set': {
+                        'unistorage_type': new_unistorage_type,
+                        'extra': new_extra,
+                    }
                 }
-            }
-            db.fs.files.update({'_id': _id}, set_spec)
-            print '  Fixed'
+                db.fs.files.update({'_id': _id}, set_spec)
+                print '  Fixed'
         else:
             print 'OK'
+
+    log.close()
