a3995ce9f8b50be8a67d82f8eecfdc494ad813f7
Anton Romanovich <anthony.romanovich@gmail.com>
2013-01-18 16:15:19 +0600

    Fix

--- actions/handlers.py
+++ actions/handlers.py
@@ -139,5 +139,6 @@ def apply_action(source_file, args):
 
     cleaned_args = action.validate_and_get_args(args, source_file=source_file)
     label = '_'.join(map(str, [action.name] + cleaned_args))
+    label = label.replace('.', '_')
     action_list = [(action.name, cleaned_args)]
     return apply_actions(source_file, action_list, label)
