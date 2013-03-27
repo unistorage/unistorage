137e4fc69b6e8da15e905c562eb538178a4db85a
Anton Romanovich <anthony.romanovich@gmail.com>
2013-03-27 17:31:43 +0600

    Enhance migration

--- migration02.py
+++ migration02.py
@@ -1,7 +1,7 @@
 """
 Usage:
 >>> from migration02 import get_callback
->>> generic_migrate({}, get_callback(actions_to_redo=['resize']))
+>>> generic_migrate({}, get_callback(actions_to_redo=['watermark']))
 """
 from bson import ObjectId
 from celery import chain
@@ -11,36 +11,37 @@ from app import db
 from actions.tasks import perform_actions
 
 
-def get_linearized_tree(file_, actions_to_redo):
-    redo_needed = False
+def get_linearized_tree(file_, actions_to_redo, redo_needed):
     chain_tasks = []
-    if file_.get('original'):
-        chain_tasks = [perform_actions.si(file_['_id'])]
-
-    modifications = file_.get('modifications', {})
-
-    for child_id in modifications.values():
-        child = db.fs.files.find_one({'_id': child_id})
-        linearized_tree, redo_needed = get_linearized_tree(child, actions_to_redo)
-        chain_tasks.extend(linearized_tree)
-        db.fs.files.update({'_id': child_id}, {
-           '$set': {
-                'pending': True,
-                'ttl': int(settings.AVERAGE_TASK_TIME.total_seconds()),
-            }
-        })
     
     actions = file_.get('actions', [])
     for action in actions:
         if action[0] in actions_to_redo:
             redo_needed = True
 
+    if redo_needed:
+        chain_tasks.append(perform_actions.si(file_['_id']))
+
+    modifications = file_.get('modifications', {})
+
+    for child_id in modifications.values():
+        child = db.fs.files.find_one({'_id': child_id})
+        linearized_tree, redo_needed = get_linearized_tree(child, actions_to_redo, redo_needed)
+        if redo_needed:
+            chain_tasks.extend(linearized_tree)
+        db.fs.files.update({'_id': child_id}, {
+           '$set': {
+                'pending': True,
+                'ttl': int(settings.AVERAGE_TASK_TIME.total_seconds()),
+            }
+        })
+
     return chain_tasks, redo_needed
 
 def get_callback(actions_to_redo):
     def callback(id_, file_, log=None):
         if not file_.get('original'):
-            chain_tasks, redo_needed = get_linearized_tree(file_, actions_to_redo)
+            chain_tasks, redo_needed = get_linearized_tree(file_, actions_to_redo, False)
             if redo_needed and chain_tasks:
                 print '  %i tasks enqueued' % len(chain_tasks)
                 chain(*chain_tasks)()
