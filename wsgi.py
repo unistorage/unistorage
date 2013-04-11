541388ffd5a2a7700766fe0392c455d30569a1cc
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-11 17:08:19 +0600

    Integrate New Relic

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,11 @@
 from app import create_app
 
+
 app = create_app()
+
+try:
+    import newrelic.agent
+    newrelic.agent.initialize('newrelic.ini')
+    app = newrelic.agent.wsgi_application()(app)
+except:
+    pass
