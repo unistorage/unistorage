996b7ed2d49a877081c54da16af21e5f5e355cdb
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-11 17:47:12 +0600

    Fix New Relic integration

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,6 @@ app = create_app()
 try:
     import newrelic.agent
     newrelic.agent.initialize('newrelic.ini')
-    app = newrelic.agent.wsgi_application()(app)
+    app.wsgi_app = newrelic.agent.wsgi_application()(app.wsgi_app)
 except:
     pass
