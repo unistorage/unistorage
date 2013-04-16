89c504ed6caa4ae1b85bea30d01b504828451d49
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-16 13:29:35 +0600

    Don't use pipe with imagemagick

--- actions/images/watermark.py
+++ actions/images/watermark.py
@@ -32,20 +32,20 @@ def perform(source_file, watermark_file, w, h, h_pad, v_pad, corner):
     watermark_bbox_geometry = '%dx%d+%d+%d' % get_watermark_bbox_geometry(
         source_data['width'], source_data['height'], w, h, h_pad, v_pad)
 
-    fd_in, fd_out = os.pipe()
+    fd = os.tmpfile()
     args = [
         settings.CONVERT_BIN, '-', '-coalesce', '-gravity', CORNER_MAP[corner],
         '-geometry', watermark_bbox_geometry, 'null:',
-        '%s:fd:%d' % (watermark_format, fd_in), '-layers', 'composite', '-'
+        '%s:fd:%d' % (watermark_format, fd.fileno()), '-layers', 'composite', '-',
     ]
+    fd.write(watermark_file.read())
+    fd.seek(0)
     proc = subprocess.Popen(
         args, stdin=subprocess.PIPE,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    os.write(fd_out, watermark_file.read())
-    os.close(fd_out)
-
     stdout_data, stderr_data = proc.communicate(input=source_file.read())
-    os.close(fd_in)
+    fd.close()
+
     if proc.returncode != 0:
         raise ActionException('`composite` failed: %s' % stderr_data)
     return StringIO(stdout_data), source_data['format']
