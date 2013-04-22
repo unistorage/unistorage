44b20ee1c1a58e4219f042d80df52f608fa7e999
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-22 18:27:37 +0600

    Bump CI

--- settings_test.py
+++ settings_test.py
@@ -2,9 +2,8 @@ DEBUG = True
 TESTING = True
 
 SENTRY_DSN = None
+SERVER_NAME = None
 
 MONGO_DB_NAME = 'grid_fs_test'
 MONGO_REPLICATION_ON = False
 CELERY_BROKER = 'mongodb://localhost:27017/q/'
-
-SERVER_NAME = None
