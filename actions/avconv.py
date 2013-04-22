a7900b37a4a836f4a718f16da322f4757e9d3cd8
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-22 15:34:29 +0600

    Migrate to static build of ffmpeg

--- actions/avconv.py
+++ actions/avconv.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 import re
 import json
 import pipes
@@ -342,8 +342,8 @@ encoder_args = {
     'vcodecs': {
         'h263p': ['-threads', '1', '-vf', 'scale=trunc(iw/4)*4:trunc(ih/4)*4'],
         'libtheora': ['-qscale', '6'],
-        'libx264': ['-flags', '+loop', '-cmp', '+chroma', '-partitions',
-                    '+parti4x4+partp8x8+partb8x8', '-subq', '5', '-trellis', '1', '-refs', '1',
+        'libx264': ['-flags', '+loop', '-cmp', 'chroma', '-partitions',
+                    'parti4x4+partp8x8+partb8x8', '-subq', '5', '-trellis', '1', '-refs', '1',
                     '-coder', '0', '-me_range', '16', '-g', '300', '-keyint_min', '25',
                     '-sc_threshold', '40', '-i_qfactor', '0.71', '-rc_eq', "'blurCplx^(1-qComp)'",
                     '-qcomp', '0.6', '-qmin', '10', '-qmax', '51', '-qdiff', '4', '-level', '30',
