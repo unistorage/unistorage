ea3f83590d762892180737909c55ddb876714a03
Anton Romanovich <anthony.romanovich@gmail.com>
2013-04-24 12:58:20 +0600

    Update migrations.m02 to accept callable as criteria for redoing

--- migrations/m02.py
+++ migrations/m02.py
@@ -2,7 +2,7 @@
 """
 Usage:
 >>> from migration02 import get_callback
->>> generic_migrate({}, get_callback(actions_to_redo=['watermark']))
+>>> generic_migrate({}, get_callback(criteria=['watermark']))
 """
 from bson import ObjectId
 from celery import chain
@@ -12,13 +12,26 @@ from app import db
 from actions.tasks import perform_actions
 
 
-def get_linearized_tree(file_, actions_to_redo, redo_needed):
+def is_redo_needed(file_, criteria):
+    """
+    :param file_: словарь с информацией о файле из Mongo,
+    :param criteria: список названий операций, подлежащих перезапуску,
+                     либо callable, принимающий `file_`, и возвращающий
+                     True, если перезапуск необходим.
+    """
+    if isinstance(criteria, list):
+        actions = file_.get('actions', [])
+        for action in actions:
+            if action[0] in criteria:
+                return True
+        return False
+    elif callable(criteria):
+        return criteria(file_)
+
+
+def get_linearized_tree(file_, criteria, redo_needed):
     chain_tasks = []
-    
-    actions = file_.get('actions', [])
-    for action in actions:
-        if action[0] in actions_to_redo:
-            redo_needed = True
+    redo_needed = is_redo_needed(file_, criteria)
 
     if redo_needed:
         chain_tasks.append(perform_actions.si(file_['_id']))
@@ -28,23 +41,24 @@ def get_linearized_tree(file_, actions_to_redo, redo_needed):
     result = redo_needed
     for child_id in modifications.values():
         child = db.fs.files.find_one({'_id': child_id})
-        linearized_tree, tree_redo_needed = get_linearized_tree(child, actions_to_redo, redo_needed)
+        linearized_tree, tree_redo_needed = get_linearized_tree(child, criteria, redo_needed)
         if tree_redo_needed:
             chain_tasks.extend(linearized_tree)
             result = result or tree_redo_needed
             db.fs.files.update({'_id': child_id}, {
-               '$set': {
+                '$set': {
                     'pending': True,
                     'ttl': int(settings.AVERAGE_TASK_TIME.total_seconds()),
-                }
+                },
             })
 
     return chain_tasks, result
 
-def get_callback(actions_to_redo):
+
+def get_callback(criteria):
     def callback(id_, file_, log=None):
         if not file_.get('original'):
-            chain_tasks, redo_needed = get_linearized_tree(file_, actions_to_redo, False)
+            chain_tasks, redo_needed = get_linearized_tree(file_, criteria, False)
             if redo_needed and chain_tasks:
                 print '  %i tasks enqueued' % len(chain_tasks)
                 chain(*chain_tasks)()
