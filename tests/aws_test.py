5c7de8b456e9ed1ef61074e9ab3580e84eab8ade
Max Pomazuev <max.pomazuev@gmail.com>
2014-12-10 17:09:41 +0500

    fixing test mocks

--- tests/aws_test.py
+++ tests/aws_test.py
@@ -8,18 +8,29 @@ import httpretty
 
 from tests.utils import StorageFunctionalTest
 from app.aws import get_aws_credentials, put_file, get_file, AWSRegularFile
+from app.models import User
 
 
 ac = {'aws_access_key_id': 'AKIAAAAAAAAAAAAAAUYA',
       'aws_secret_access_key': 'RVddd+asdfDFDSdfsSDFSDFdfDFDSFdfsdfdUwJp',
-      'aws_bucket_name': 'unistorage1'}
+      'aws_bucket_name': 'unistorage'}
 
 
 class FunctionalTest(StorageFunctionalTest):
+    @mock_s3
     def test_AWSRegularFile(self):
+        conn = boto.connect_s3(ac['aws_access_key_id'], ac['aws_secret_access_key'])
+        conn.create_bucket(ac['aws_bucket_name'])
+
         f = open('tests/fixtures/audios/god-save-the-queen.mp3', 'r')
         from app import db, fs
-        AWSRegularFile.put_to_fs(db, fs, 'asdf', f, ac, user_id=ObjectId('5464622fe5653b41120ddc5f'))
+        user_id = User.get_one(db, {}).get_id()
+        file_id = AWSRegularFile.put_to_fs(db, fs, 'asdf', f, ac, user_id=user_id)
+
+        grid_out_file = AWSRegularFile.get_from_fs(db, fs, _id=file_id)
+        self.assertEqual(grid_out_file.length, 0)
+        self.assertEqual(grid_out_file.aws_bucket_name, ac['aws_bucket_name'])
+        self.assertEqual(grid_out_file.aws_size, 1186681)
 
     def test_api_aws_url(self):
         # Заливаем файл в GridFS
@@ -87,12 +98,12 @@ class FunctionalTest(StorageFunctionalTest):
     @mock_s3
     def test_put_file(self):
         conn = boto.connect_s3(ac['aws_access_key_id'], ac['aws_secret_access_key'])
-        conn.create_bucket('unistorage2')
+        conn.create_bucket(ac['aws_bucket_name'])
 
         with open('tests/fixtures/asdf.txt', 'r') as f:
             length = put_file(ac, 'asdf', f)
 
-        self.assertEqual(conn.get_bucket('unistorage2').get_key('asdf').get_contents_as_string(), 'asdfasdf')
+        self.assertEqual(conn.get_bucket('unistorage').get_key('asdf').get_contents_as_string(), 'asdfasdf')
         self.assertEqual(length, 8)
 
     @httpretty.httprettified
