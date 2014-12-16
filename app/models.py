0af2512d2ecc9b6edbe446fabf5680c8f2e8163f
Max P <max.pomazuev@gmail.com>
2014-12-16 11:51:39 +0500

    Revert "updating statistics updates for sharding"

--- app/models.py
+++ app/models.py
@@ -462,7 +462,7 @@ class RegularFile(File):
                 'files_count': 1,
                 'files_size': fs.get(file_id).length,
             }
-        }, multi=True, upsert=True)
+        }, upsert=True)
         return file_id
 
 
