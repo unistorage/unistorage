e38286cc41a5153b8a200ae11e57529103dc71b1
Max Pomazuev <max.pomazuev@gmail.com>
2016-01-04 15:22:17 +0500

    pause option for delete_user_files

--- commands/delete_user_files.py
+++ commands/delete_user_files.py
@@ -1,13 +1,15 @@
 # coding: utf-8
 import sys
+from time import sleep
 from app import db
 from wsgi import app
 from app.models import File, User, Statistics
 
 
-def delete_user_files(user_token, statistics_delete=False, number_of_querying_files='100', query=False, block=False):
+def delete_user_files(user_token, statistics_delete=False, number_of_querying_files='100', query=False, block=False, pause=None):
     """'-b' marks user as 'blocked', then deletes files. With '-s' deletes user
     statistics too. Use '-n NUMBER' to set number of files in query to delete.
+    Use '-p' for specifying time in seconds to sleep per operations.
     Use '-q' for specifying additional query
     """
     # Flask-Script неявно пушит application context, поэтому мы можем использовать db
@@ -54,6 +56,9 @@ def delete_user_files(user_token, statistics_delete=False, number_of_querying_fi
 
         sys.stdout.write('.')
         sys.stdout.flush()
+
+        if pause:
+            sleep(int(pause))
 
     if statistics_delete:
         sys.stdout.write('Deleting statistics...')
